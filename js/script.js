aa68fe52ff4643920ef4c09db0ef3f4af56b0724
Alexey Fetisov <alfet123@gmail.com>
2017-01-12 23:55:28 +0500

    Окно для картинки, оформление кнопок и меню

--- js/script.js
+++ js/script.js
@@ -4,7 +4,14 @@
   var mainMenu = document.querySelector('.main-menu');
   var stickedMenu = document.querySelector('.sticked-menu');
 
-  window.addEventListener('scroll', function(evt){
+  var catalog = document.querySelector('.catalog');
+  var images = catalog.querySelectorAll('img');
+  var modal = document.querySelector(".modal");
+  var closeBtn = modal.querySelector(".modal__close-btn");
+
+// Переключение меню
+
+  var switchMenu = function() {
     headerBottom = header.getBoundingClientRect().bottom;
     if (headerBottom < 1) {
       mainMenu.classList.add('blanked');
@@ -12,6 +19,35 @@
     } else {
       mainMenu.classList.remove('blanked');
       stickedMenu.classList.add('hidden');
+    }
+  }
+
+  window.addEventListener('scroll', switchMenu);
+
+  switchMenu();
+
+// Отображение галереи
+
+  var showGallary = function(evt) {
+    if (!modal.classList.contains("modal--show")) {
+      modal.classList.add("modal--show");
+      window.console.log(evt.target.attributes.src.value);
+    }
+  }
+
+  for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', showGallary);
+  }
+
+  closeBtn.addEventListener("click", function() {
+    modal.classList.remove("modal--show");
+  });
+
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode == 27) {
+      if (modal.classList.contains("modal--show")) {
+        modal.classList.remove("modal--show");
+      }
     }
   });
 
